9b951197088c0919f88bbf2997b5190956528dc8
Akshata1302 <student-04-790cf735d09f@qwiklabs.net>
2026-06-16 08:50:01 +0000

    Dev v2.0 with red endpoint

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 /**
  * Copyright 2023 Google Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+* Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -22,16 +22,16 @@ import (
 	"image/draw"
 	"image/png"
 	"net/http"
-)
-
+}
 func main() {
-	http.HandleFunc("/blue", blueHandler)
-	http.ListenAndServe(":8080", nil)
+    http.HandleFunc("/blue", blueHandler)
+    http.HandleFunc("/red", redHandler)
+    http.ListenAndServe(":8080", nil)
 }
 
-func blueHandler(w http.ResponseWriter, r *http.Request) {
-	img := image.NewRGBA(image.Rect(0, 0, 100, 100))
-	draw.Draw(img, img.Bounds(), &image.Uniform{color.RGBA{0, 0, 255, 255}}, image.ZP, draw.Src)
-	w.Header().Set("Content-Type", "image/png")
-	png.Encode(w, img)
+func redHandler(w http.ResponseWriter, r *http.Request) {
+    img := image.NewRGBA(image.Rect(0, 0, 100, 100))
+    draw.Draw(img, img.Bounds(), &image.Uniform{color.RGBA{255, 0, 0, 255}}, image.ZP, draw.Src)
+    w.Header().Set("Content-Type", "image/png")
+    png.Encode(w, img)
 }
